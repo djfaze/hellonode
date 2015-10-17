10546311af4e0b489db9e0d00376aa38626235d7
anandkumarpatel <anand@runnable.com>
2015-10-16 17:44:30 -0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-    res.send('runnable3');
+    res.send('AntonAndAnandAreAmazing');
 });
 
 app.listen(80);
